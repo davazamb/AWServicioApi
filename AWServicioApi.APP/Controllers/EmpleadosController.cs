b1e283a2be9ae8ab136bf499b440633c56d73645
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-12-08 11:23:16 -0300

    Instalacion del Entity Framework en proyecto Api, incluyendo los metodos Get y Get(id) para la consulta

--- AWServicioApi.APP/Controllers/EmpleadosController.cs
+++ AWServicioApi.APP/Controllers/EmpleadosController.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AWServicioApi.AD;
 
 namespace AWServicioApi.APP.Controllers
 {
     public class EmpleadosController : ApiController
     {
-
+        public IEnumerable<Employees> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+        public Employees Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
     }
 }
